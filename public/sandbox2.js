b0f6f70acc7749bdc3648bd3464229867c1cdd4c
Pelc314 <mpepelc@gmail.com>
2023-05-07 20:27:23 +0200

    more info about functions

--- public/sandbox2.js
+++ public/sandbox2.js
@@ -5,7 +5,14 @@ let greet = () => {
 };
 let greet2;
 greet2 = () => { console.log('hello world'); };
-const add = (a, b) => {
+// argument with a question mark is optional, default value also makes a parameter optional.
+// this returns void
+const add = (a, b, c, d = '10') => {
     console.log(a + b);
 };
 add(5, 10);
+const plus = (a, b) => {
+    return a + b;
+};
+let result = plus(10, 7);
+console.log(result);
--- src/sandbox2.ts
+++ src/sandbox2.ts
@@ -7,8 +7,18 @@ let greet2: Function;
 
 greet2 = () => { console.log('hello world') }
 
-const add = (a: number, b:number)=>{
+// argument with a question mark is optional, default value also makes a parameter optional.
+// this returns void
+const add = (a: number, b: number, c?: number, d: string = '10') => {
     console.log(a + b);
 }
 
-add(5,10);
+add(5, 10);
+
+const plus = (a: number, b: number): number => {
+    return a + b;
+}
+
+let result = plus(10, 7);
+
+console.log(result);
